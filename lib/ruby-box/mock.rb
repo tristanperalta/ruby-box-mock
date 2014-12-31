124cab41242ffa2088255b75bf7967475a052cb8
Tristan Peralta <tristanperalta@gmail.com>
2014-12-31 19:07:02 +0800

    Add helper methods to stub all folder requests

--- lib/ruby-box/mock.rb
+++ lib/ruby-box/mock.rb
@@ -2,3 +2,13 @@ require "ruby-box"
 require "webmock"
 require "ruby-box/mock/version"
 require "ruby-box/mock/folder"
+
+module RubyBox
+  module Mock
+    def self.mock
+      folder = Folder.new
+      folder.stub_folder_requests
+      folder.stub_post_folder_request
+    end
+  end
+end
